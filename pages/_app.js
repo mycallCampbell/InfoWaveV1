4d02b6c45ed589a5185afa08a836186e72abd693
unknown <mycallcampbell@gmail.com>
2023-04-07 11:05:04 +0100

    updated _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Layout from "../components/Layout";
-import { ProductProvider } from "../productPageContext";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-
-
-const stripePromise = loadStripe(
-  "pk_live_51JgseNAB9i0B8xShz7YQSfnBjD0ayARQRcA2YGxo6BJPtdrwLPqUE3VVYahGFT5f2rGA8c3g6s0DIVQ7h5azyqJF00GKRrNcTd"
-);
 
 function MyApp({ Component, pageProps }) {
-return (
-  <Elements stripe={stripePromise}>
-      <ProductProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ProductProvider>
-    </Elements>
-)
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
